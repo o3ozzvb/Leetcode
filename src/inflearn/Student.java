6c36c4652d33df95ed6463844b4f8a9244c96a60
oeunkyoung <ou1112@naver.com>
2021-10-13 21:24:43 +0900

    .

--- src/inflearn/Student.java
+++ src/inflearn/Student.java
@@ -1,6 +1,5 @@
 package inflearn;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Student {
@@ -8,22 +7,22 @@ public class Student {
 	public int solution(int n, int[] arr) {
 		int answer = 1;
 		int tallest = arr[0];
-		for(int i = 1; i < n; i++) {
-			if(tallest < arr[i]) {
+		for (int i = 1; i < n; i++) {
+			if (tallest < arr[i]) {
 				tallest = arr[i];
 				answer++;
 			}
 		}
-		
+
 		return answer;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int[] arr = new int[n];
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = in.nextInt();
 		}
 		Student st = new Student();
