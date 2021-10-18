21d6dcea3fe677e9dc85d9e00a9ed01f5745ba94
oeunkyoung <ou1112@naver.com>
2021-10-18 21:50:10 +0900

    inflearn : 등수 구하기

---
+++ src/inflearn/GetRanking.java
@@ -0,0 +1,38 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class GetRanking {
+
+	public int[] solution(int n, int[] score) {
+		int[] answer = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			int rank = 1;
+			for(int j=0; j<n; j++) {
+				if(i != j && score[i] < score[j]) {
+					rank++;
+				}
+			}
+			answer[i] = rank;
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		GetRanking gr = new GetRanking();
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] score = new int[n];
+		int max = 0;
+		for(int i = 0; i < n; i++) {
+			score[i] = in.nextInt();
+		}
+		
+		for(int x : gr.solution(n, score)) {
+			System.out.print(x + " ");
+		}
+	}
+
+}
