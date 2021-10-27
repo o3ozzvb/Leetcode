3690ba34320d922582b4fefd3ac351f6d0c4dd18
oeunkyoung <ou1112@naver.com>
2021-10-27 18:03:33 +0900

    inflearn : 학급 회장

---
+++ src/inflearn/ClassPresident.java
@@ -0,0 +1,38 @@
+package inflearn;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Scanner;
+
+public class ClassPresident {
+
+	public char solution(int n, String s) {
+		char answer = ' ';
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for(char c : s.toCharArray()) {
+			map.replace(c, map.getOrDefault(c,0)+1);
+		}
+		
+		int max = Integer.MIN_VALUE;
+		for(Entry<Character, Integer> e : map.entrySet()) {
+			if(max < e.getValue()) {
+				max = e.getValue();
+				answer = e.getKey();
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPresident cp = new ClassPresident();
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.nextInt();
+		String s = in.next();
+		
+		System.out.println(cp.solution(n, s));
+	}
+
+}
