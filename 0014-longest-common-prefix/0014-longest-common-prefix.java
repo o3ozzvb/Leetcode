e7157186a0a55aeeef45038761132da5f4546822
Eunkyoung Oh <ou1112@naver.com>
2023-02-19 22:40:20 +0900

    Time: 1 ms (74.41%), Space: 40.3 MB (60.69%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,6 +1,7 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) return "";
+        
         for (int i = 0; i < strs[0].length() ; i++){
             char c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j ++) {
@@ -8,6 +9,7 @@ class Solution {
                     return strs[0].substring(0, i);
             }
         }
+        
         return strs[0];
     }
 }
