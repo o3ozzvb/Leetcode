d1d1ee3377cc14144b5a4024e86008fa5e99e893
Eunkyoung Oh <ou1112@naver.com>
2023-07-02 21:05:33 +0900

    Time: 3 ms (6.78%), Space: 42.5 MB (32.89%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        boolean lastP = true;
+        List<Integer> list = new LinkedList<>();
+
+        for (int i = nums.length - 1; i > 0; i--) {
+            list.add(nums[i]);
+
+            if (nums[i - 1] < nums[i]) {
+                lastP = false;
+
+                /*
+                int tmp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = tmp;
+                */
+
+//                pq.add(nums[i-1]);
+
+                list.sort(Comparator.naturalOrder());
+
+                for (int j = 0; j < list.size(); j++) {
+                    if (nums[i - 1] < list.get(j)) {
+                        int x = list.get(j);
+                        list.remove(j);
+                        list.add(nums[i - 1]);
+                        nums[i - 1] = x;
+                        break;
+                    }
+                }
+
+                list.sort(Comparator.naturalOrder());
+                for (int j = 0; j < list.size(); j++) {
+                    nums[i + j] = list.get(j);
+                }
+
+                return;
+            }
+        }
+        if (lastP) {
+            Arrays.sort(nums);
+        }
+    }
+}
